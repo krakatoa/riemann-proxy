be0e13d0a5b4b6d24fa03a696a109250a7f370c0
Fernando Alonso <krakatoa1987@gmail.com>
2016-04-15 23:20:42 -0300

    Refactor on Ping module

--- lib/riemann_proxy/ping.ex
+++ lib/riemann_proxy/ping.ex
@@ -1,12 +1,11 @@
 defmodule RiemannProxy.Ping do
   def ping(host, port) do
-    case :gen_tcp.connect(host, port, [:binary, packet: 4, active: false, send_timeout: 2000], 2000) do
-      {:ok, socket} ->            try_send(socket)
-      {:error, _reason} ->        false
-    end
+    :gen_tcp.connect(host, port, [:binary, packet: 4, active: false, send_timeout: 2000], 2000)
+    |> try_send
   end
 
-  defp try_send(socket) do
+  defp try_send({:error, _reason}), do: false
+  defp try_send({:ok, socket}) do
     query = RiemannProxy.Proto.Query.new(string: 'service = "riemann.ping"')
     msg = RiemannProxy.Proto.Msg.encode(RiemannProxy.Proto.Msg.new(query: query))
     :ok = :gen_tcp.send(socket, msg)
